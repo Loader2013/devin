Indie365/OpenDevin

add workspace setting in ui

frontend/src/api/index.ts
@@ -7,3 +7,8 @@ export async function fetchAgents() {
   const response = await fetch(`/api/agents`);
   return response.json();
 }
+
+export async function fetchWorkspaceDirs() {
+  const response = await fetch(`/api/workspace-dirs`);
+  return response.json();
+}

Loader2013/devin

frontend/src/api/index.ts
@@ -7,3 +7,8 @@ export async function fetchAgents() {
   const response = await fetch(`/api/agents`);
   return response.json();
 }
+
+export async function fetchWorkspaceSubdirs() {
+  const response = await fetch(`/api/workspace-subdirs`);
+  return response.json();
+}